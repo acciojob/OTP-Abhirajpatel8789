5f6efe0917843d72b4842e3cdce4eb11a3290e39
Abhirajpatel8789 <122907724+Abhirajpatel8789@users.noreply.github.com>
2026-03-17 18:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-//your JS code here. If required.
-let inputs = document.querySelectorAll(".code");
-        inputs.forEach((input,index)=>{
-            input.addEventListener("input",(e)=>{
-                 let value = e.target.value;
+const inputs = document.querySelectorAll(".code");
 
-                 if(isNaN(value)){
-                    e.target.value
-                    return;
-                 }
-                 if(value && index < inputs.length-1){
-                    inputs[index+1].focus()
-                 }
-            })
 
-            input.addEventListener("keydown",(e)=>{
-                if(e.key == "Backspace" && !input.value && index > 0 ){
-                    inputs[index-1].focus()
-                }
-            })
-            
-        })
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+
+  input.addEventListener("input", (e) => {
+    let value = e.target.value;
+
+    
+    if (!/^[0-9]$/.test(value)) {
+      e.target.value = "";
+      return;
+    }
+
+     
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+
+    
+    if (e.key === "Backspace") {
+      
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+      }
+      
+      
+      input.value = "";
+    }
+
+  });
+
+});
